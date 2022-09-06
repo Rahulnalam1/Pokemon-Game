0d704b9aa3d8f3f27f0f4010bfa74684d8716f6c
Rahul Nalam <rahulnalam1@gmail.com>
2022-09-06 02:23:17 -0400

    added boundary collisions

--- PokemonWEBProject/index.js
+++ PokemonWEBProject/index.js
@@ -141,7 +141,8 @@ function animate() {
     player.draw()
  
 
-    if(player.position.x + player.width >= testBoundary.position.x && player.position.x <= testBoundary.position.x + testBoundary.width) { 
+    if(player.position.x + player.width >= testBoundary.position.x && player.position.x <= testBoundary.position.x + testBoundary.width
+        && player.position.y <= testBoundary.position.y + testBoundary.position.height && player.position.y + player.height >= testBoundary.position.y) { 
         console.log("colliding");
     }
 
